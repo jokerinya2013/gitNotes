f03437986a188fddb2372b94d2c3e256f1d257a4
ibrahim <ibrahim@unico.aero>
2020-01-17 12:03:20 +0300

    Slayt vue çalışıyor, datajs bilgileri alınıyor

--- my-slider/src/slayts/Slayt.vue
+++ my-slider/src/slayts/Slayt.vue
@@ -1,13 +1,56 @@
 <template>
-  <div class="row mt-5 align-items-center">
-    <div class="col text-center">
-      <img src="@/assets/logo2.png" class="img-fluid" alt="Responsive image" />
+  <div>
+    <div class="row mt-5 align-items-center">
+      <p>{{info[i].bas}}</p>
+    </div>
+
+    <div class="mt-5 slayt_footer">
+      <button type="button" class="btn btn-outline-secondary float-left" @click="nav('prev')">Önceki</button>
+      <button
+        type="button"
+        class="btn btn-outline-secondary float-right"
+        @click="nav('next')"
+      >Sonraki</button>
     </div>
   </div>
 </template>
 
 <script>
 /* burada js de yer alacak olan bilgiler dönecek. */
-export default {};
+import { info } from "./data";
+export default {
+  name: "Slayt",
+  data() {
+    return {
+      info: info,
+      i: 0
+    };
+  },
+  methods: {
+    nav(side) {
+      if (side === "prev") {
+        if (this.i === 0) {
+          this.i = 0;
+        } else {
+          this.i--;
+        }
+      } else {
+        if (this.i === this.info.length - 1) {
+          this.i = this.info.length - 1;
+        } else {
+          this.i++;
+        }
+      }
+
+      console.log(this.i);
+    }
+  }
+};
 </script>
 
+<style scoped>
+.slayt_footer {
+  border-top: 1px dotted #cccccc;
+  padding-top: 1rem;
+}
+</style>
---
+++ my-slider/src/slayts/data.js
@@ -0,0 +1,4 @@
+export const info = [
+  { bas: "Deneme1", bilgi: "Naber nasılsın" },
+  { bas: "Deneme2", bilgi: "bu ikinci sınrada" }
+];
