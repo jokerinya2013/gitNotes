4d4e42f30bf3d54c7ac17b549dc14880a78ca1d2
ibrahim <ibrahim@unico.aero>
2020-01-17 17:31:51 +0300

    sistem çalışıyor makyaj yapılacak

--- my-slider/src/components/Header.vue
+++ my-slider/src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
-  <nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand font-weight-bold" href="/">Home</a>
-  </nav>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand font-weight-bold" href="/">Ana Sayfa</a>
+    </nav>
 </template>
 
 <script>
 export default {
-  name: "app-header"
-};
+    name: 'app-header',
+}
 </script>
--- my-slider/src/slayts/Slayt.vue
+++ my-slider/src/slayts/Slayt.vue
@@ -1,56 +1,87 @@
 <template>
-  <div>
-    <div class="row mt-5 align-items-center">
-      <p>{{info[i].bas}}</p>
-    </div>
+    <div>
+        <div class="row mt-5 align-items-center">
+            <table class="table" style="height: 30rem;">
+                <thead>
+                    <tr>
+                        <th scope="col" class="text-center baslik">{{ info[i].bas }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-center bilgi">{{ info[i].bilgi }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <h5 class="float-right">{{ i + 1 }}/{{ info.length }}</h5>
+        </div>
 
-    <div class="mt-5 slayt_footer">
-      <button type="button" class="btn btn-outline-secondary float-left" @click="nav('prev')">Önceki</button>
-      <button
-        type="button"
-        class="btn btn-outline-secondary float-right"
-        @click="nav('next')"
-      >Sonraki</button>
+        <div class="mt-5 slayt_footer">
+            <button
+                type="button"
+                class="btn btn-outline-secondary float-left"
+                :class="{ disabled: son }"
+                @click="nav('prev')"
+            >
+                Önceki
+            </button>
+
+            <button
+                type="button"
+                class="btn btn-outline-secondary float-right"
+                :class="{ disabled: son }"
+                @click="nav('next')"
+            >
+                Sonraki
+            </button>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 /* burada js de yer alacak olan bilgiler dönecek. */
-import { info } from "./data";
+import { info } from './data'
 export default {
-  name: "Slayt",
-  data() {
-    return {
-      info: info,
-      i: 0
-    };
-  },
-  methods: {
-    nav(side) {
-      if (side === "prev") {
-        if (this.i === 0) {
-          this.i = 0;
-        } else {
-          this.i--;
+    name: 'Slayt',
+    data() {
+        return {
+            info: info,
+            i: 0,
+            son: false,
         }
-      } else {
-        if (this.i === this.info.length - 1) {
-          this.i = this.info.length - 1;
-        } else {
-          this.i++;
-        }
-      }
+    },
+    methods: {
+        nav(side) {
+            if (side === 'prev') {
+                if (this.i === 0) {
+                    this.i = 0
+                } else {
+                    this.i--
+                }
+            } else {
+                if (this.i === this.info.length - 1) {
+                    this.i = this.info.length - 1
+                } else {
+                    this.i++
+                }
+            }
 
-      console.log(this.i);
-    }
-  }
-};
+            console.log(this.i)
+        },
+    },
+    //son u düzenleyeceksin
+}
 </script>
 
 <style scoped>
+.baslik {
+    font-size: 2rem;
+}
+.bilgi {
+    font-size: 1.5rem;
+}
 .slayt_footer {
-  border-top: 1px dotted #cccccc;
-  padding-top: 1rem;
+    border-top: 1px dotted #cccccc;
+    padding-top: 1rem;
 }
 </style>
